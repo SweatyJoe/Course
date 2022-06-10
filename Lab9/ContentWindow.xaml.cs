f9a6915d96100c0cd99a7d61a5dac5861d4189a9
telenchenko_andrey <telenchenko15@gmail.com>
2022-06-10 17:35:10 +0300

    lab9_v1

---
+++ Lab9/ContentWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab9
+{
+    /// <summary>
+    /// Логика взаимодействия для ContentWindow.xaml
+    /// </summary>
+    public partial class ContentWindow : Window
+    {
+        private string fileContent = string.Empty;
+        private string filePath = string.Empty;
+        public ContentWindow(string fileContent, string filePath)
+        {
+            InitializeComponent();
+            this.fileContent = fileContent;
+            this.filePath = filePath;
+            contentBox.Text = fileContent;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool ok = isOk();
+            if (ok)
+            {
+                fileContent = contentBox.Text;
+                MessageBox.Show("Изменения сохранены успешно", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        protected static bool isOk()
+        {
+            if (MessageBox.Show("Точно сохранить? Результат может быть необратим", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                return false;
+            else
+                return true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(MessageBox.Show("Выйти? Не сохранённые изменения будут утеряны!", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                MainWindow mainW = new MainWindow();
+                mainW.Show();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            contentBox.Text = fileContent;
+            MessageBox.Show("Изменения загружены", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    await writer.WriteLineAsync(fileContent);
+                }
+                MessageBox.Show("Изменения сохранены в файл успешно", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка записи в файл" + ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ Lab9/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace Lab9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileContent = string.Empty;
+        private string filePath = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+            goChangeContentButton.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+
+
+            openDialog.InitialDirectory = "C:\\";
+            openDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openDialog.FilterIndex = 2;
+            openDialog.RestoreDirectory = true;
+            if (openDialog.ShowDialog() == true)
+            {
+                filePath = openDialog.FileName;
+                var fileStream = openDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+            }
+            if (filePath != "") goChangeContentButton.Visibility = Visibility.Visible;
+            else MessageBox.Show("Не выбран файл");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ContentWindow cw = new ContentWindow(fileContent, filePath);
+            cw.Show();
+            this.Hide();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(MessageBox.Show("Выйти?", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Hand) == MessageBoxResult.Yes)
+            {
+                //
+            }
+        }
+    }
+}
